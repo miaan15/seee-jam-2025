2f866f4241305e69a6756fd00a42293b1209aa33
miaan15 <miaanstarman@gmail.com>
2025-09-18 07:35:15 +0700

    click inventory

---
+++ Assets/_Main/Scripts/UI/BeatInvenManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatInvenManager : MonoBehaviour
+{
+    public RectTransform BombInvenTransform;
+    public RectTransform DetoInvenTransform;
+
+    public int Num;
+    public float Space;
+    public Vector2 Size;
+
+    private Button[] _bombButtons;
+    private Button[] _detoButtons;
+
+    private void ClickBombInven(int index)
+    {
+        Debug.Log($"Bomb inventory clicked: {index}");
+    }
+
+    private void ClickDetoInven(int index)
+    {
+        Debug.Log($"Deto inventory clicked: {index}");
+    }
+
+    private void Start()
+    {
+        _bombButtons = CreateRow(
+            origin: BombInvenTransform,
+            count: Num,
+            onClick: ClickBombInven,
+            rowName: "BombBtn_"
+        );
+
+        _detoButtons = CreateRow(
+            origin: DetoInvenTransform,
+            count: Num,
+            onClick: ClickDetoInven,
+            rowName: "DetoBtn_"
+        );
+    }
+
+    private Button[] CreateRow(RectTransform origin, int count, System.Action<int> onClick, string rowName)
+    {
+        var buttons = new Button[count];
+
+        Vector3 originWorld = origin.TransformPoint(origin.rect.center);
+        Vector3 originLocal = transform.InverseTransformPoint(originWorld);
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject go = new GameObject($"{rowName}{i}", typeof(RectTransform), typeof(Image), typeof(Button));
+            go.transform.SetParent(this.transform, worldPositionStays: false);
+
+            RectTransform rt = go.GetComponent<RectTransform>();
+            rt.sizeDelta = Size;
+            rt.anchorMin = new Vector2(0.5f, 0.5f);
+            rt.anchorMax = new Vector2(0.5f, 0.5f);
+            rt.pivot = new Vector2(0.5f, 0.5f);
+
+            Vector2 pos = (Vector2)originLocal + new Vector2(i * Space, 0f);
+            rt.anchoredPosition = pos;
+
+            Image img = go.GetComponent<Image>();
+            img.raycastTarget = true;
+            img.color = new(0f, 0f, 0f, 0f);
+
+            Button btn = go.GetComponent<Button>();
+            int capturedIndex = i;
+            btn.onClick.AddListener(() => onClick?.Invoke(capturedIndex));
+
+            buttons[i] = btn;
+        }
+
+        return buttons;
+    }
+}
